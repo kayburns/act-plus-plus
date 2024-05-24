85cad5ada8490a58487e73b5886f1597a96cf7cf
Kaylee Burns <kaylee.c.burns@gmail.com>
2024-05-24 12:59:28 -0700

    run files

---
+++ launch.sh
@@ -0,0 +1,19 @@
+task_names=("sim_insertion_decoupled_scripted_GEOM0.005_SITE0.005")
+seeds=("1")
+num_episodes=("50")
+batch_sizes=("8")
+policy_classes=("ACT")
+
+for seed in ${seeds[@]} ; do
+    for task_name in ${task_names[@]} ; do
+            for num_episode in ${num_episodes[@]} ; do
+                for batch_size in ${batch_sizes[@]} ; do
+                    for policy_class in ${policy_classes[@]} ; do
+                        sbatch run.sh ${task_name} ${seed} ${num_episode} ${batch_size} ${policy_class}
+                    done
+                done
+            done
+        done
+    done
+done
+
