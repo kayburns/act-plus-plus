dfcc1abd3da00d3b38191a11030770f3208ea165
Kaylee Burns <kaylee.c.burns@gmail.com>
2024-05-25 17:23:00 -0700

    add cpus per task to launch file

--- launch.sh
+++ launch.sh
@@ -1,16 +1,15 @@
-task_names=("sim_insertion_decoupled_scripted_GEOM0.005_SITE0.005")
-seeds=("1")
+task_names=("sim_transfer_cube_decoupled_3cam_scripted_GEOM0.005_SITE0.005")
+seeds=("2" "3")
 num_episodes=("50")
 batch_sizes=("8")
-policy_classes=("ACT")
+policy_classes=("Diffusion")
 
 for seed in ${seeds[@]} ; do
     for task_name in ${task_names[@]} ; do
-            for num_episode in ${num_episodes[@]} ; do
-                for batch_size in ${batch_sizes[@]} ; do
-                    for policy_class in ${policy_classes[@]} ; do
-                        sbatch run.sh ${task_name} ${seed} ${num_episode} ${batch_size} ${policy_class}
-                    done
+        for num_episode in ${num_episodes[@]} ; do
+            for batch_size in ${batch_sizes[@]} ; do
+                for policy_class in ${policy_classes[@]} ; do
+                    sbatch run.sh ${task_name} ${seed} ${num_episode} ${batch_size} ${policy_class}
                 done
             done
         done
